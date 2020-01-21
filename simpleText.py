4479ddccadc9fdad5fb9a467517f2106d7877355
Jianny <56791347+jianny-lee@users.noreply.github.com>
2020-01-21 15:13:30 +0900

    Update simpleText.py

--- simpleText.py
+++ simpleText.py
@@ -2,9 +2,16 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def main():
-    return "Hi! If you wanna get response about Skill server, Go to /skill"
+@app.route("/test", methods=["GET"])
+def test():
+    data = jsonify(
+        version = 1.0,
+        value_list = [
+            "abc",
+            "def"
+        ]
+    )
+    return data
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
