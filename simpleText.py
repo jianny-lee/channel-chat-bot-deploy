dbb31097864ac96a6f28c68492f46ba233184e01
Jianny <56791347+jianny-lee@users.noreply.github.com>
2020-01-21 15:30:28 +0900

    Update simpleText.py

--- simpleText.py
+++ simpleText.py
@@ -1,17 +1,22 @@
 from flask import Flask, jsonify, request
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/test", methods=["GET"])
-def test():
-    data = jsonify(
-        version = 1.0,
-        value_list = [
-            "abc",
-            "def"
-        ]
-    )
-    return data
+@app.route("/skill", methods=["POST"])
+def skill():
+    data = {
+        "version" : "2.0",
+        "template" : {
+        "outputs" : [
+                {
+                    "simpleText": {
+                    "text": "This is a simple text."
+                        }
+                    }
+                ]
+            }
+        }
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
